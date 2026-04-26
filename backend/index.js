be5cbf78cf90f25f9b1cd0b739afdaf3cbde4e94
Ankit Gautam <ankitgautam6835@gmail.com>
2026-04-26 14:42:21 +0530

    Fix Billing Issue

--- backend/index.js
+++ backend/index.js
@@ -37,7 +37,7 @@ app.use(
       }
       callback(new Error('Request blocked by CORS policy'));
     },
-    methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   })
